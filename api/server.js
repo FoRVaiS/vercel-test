4f0776dc3f697cca240d1b192a13c678e9fb9a6c
Benjamin Chiang <chiangbenjamin991@gmail.com>
2024-01-13 15:34:02 -0800

    refactor: re-base all api routes to /api/v1

--- api/server.js
+++ api/server.js
@@ -1,17 +1,25 @@
+// @ts-check
 const express = require('express');
 
 function createServer(port, address) {
   const app = express();
 
-  app.get('/healthz', (req, res) => {
+  const v1Router = express.Router();
+
+  // Prefix all routes to v1Router with '/api/v1'.
+  app.use('/api/v1', v1Router);
+
+  // Use v1Router to handle all routes
+  v1Router.get('/healthz', (req, res) => {
     res.send('OK');
   });
 
-  app.get('/greet', (req, res) => {
+  v1Router.get('/greet', (req, res) => {
     const { name } = req.query;
     res.send({ msg: `Welcome ${name}!` });
   })
 
+  // Listen on the specified port and address.
   app.listen(port, address, () => {
     console.log('Hello World!');
   });
